4d345f8cf6d7b4f97eef3dc65b349bfa73ed9669
onowakow <nowakowskiowen08@gmail.com>
2022-12-05 07:31:20 -0700

    Build first component
    Create a component to understand how html, ts, and css interact

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList("Owen", [
+    new TodoItem("Cook beans", true),
+    new TodoItem("Get Shrek from library"),
+    new TodoItem("Dry oranges for garland"),
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length
+  }
 }
