4629be37e4744cb8ddccfcaaa75b8371fd517f08
onowakow <nowakowskiowen08@gmail.com>
2022-12-09 11:16:32 -0700

    Hook up REST

--- SportsStore/src/app/model/model.module.ts
+++ SportsStore/src/app/model/model.module.ts
@@ -4,14 +4,17 @@ import { ProductRepository } from './product.repository';
 import { StaticDataSource } from './static.datasource';
 import { Order } from './order.model';
 import { OrderRepository } from './order.repository';
-
+import { RestDataSource } from './rest.datasource';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
+  imports: [HttpClientModule],
   providers: [
     ProductRepository,
     StaticDataSource,
     Cart,
     Order,
     OrderRepository,
+    { provide: StaticDataSource, useClass: RestDataSource },
   ],
 })
 export class ModelModule {}
